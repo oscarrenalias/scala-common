a4671d1366174fa29067def14df65e4ad98ba93a
Oscar Renalias <oscar@renalias.net>
2011-07-13 00:11:24 +0200

    The project is now able to publish the javadoc, sources and jar packages to a maven repository

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,7 +1,19 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
-val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test"
-val log_lib = "ch.qos.logback" % "logback-classic" % "0.9.26"
-val configgy = "net.lag" % "configgy" % "2.0.0" intransitive()
+
+  // generate javadoc and source packages
+  override def packageDocsJar = defaultJarPath("-javadoc.jar")
+  override def packageSrcJar= defaultJarPath("-sources.jar")
+  val sourceArtifact = Artifact.sources(artifactID)
+  val docsArtifact = Artifact.javadoc(artifactID)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
+
+  // publish content to my local maven repository (currently a git repo in github.com)
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = Resolver.file("renalias-repo", new java.io.File("..\\phunkphorce.github.com\\maven"))
+
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test"
+  val log_lib = "ch.qos.logback" % "logback-classic" % "0.9.26"
+  val configgy = "net.lag" % "configgy" % "2.0.0" intransitive()
 }
